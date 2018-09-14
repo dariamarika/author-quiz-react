240384273ad322e08e311cfe836028747c6850f5
Daria Riabchikova <dariar@olympic.co.nz>
2018-09-14 16:01:33 +1200

    Implement check the answer

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
 import './bootstrap/css/bootstrap.min.css';
 
 function Hero() {
   return (
-    <div class="row">
+    <div className="row">
       <div className="jumbotron col-10 offset-1">
         <h1>Author Quiz</h1>
         <p>Select the book written by the author shown</p>
@@ -13,25 +14,45 @@ function Hero() {
   );
 }
 
-function Book({ title }) {
+function Book({ title, onClick }) {
   return (
-    <div className="answer">
+    <div className="answer" onClick={() => {onClick(title)}}>
       <h4>{title}</h4>
     </div>
   );
 }
 
-function Turn({ author, books }) {
+function Turn({ author, books, highlight, onAnswerSelected}) {
+  function highlightToBgColor(highlight) {
+    const mapping = {
+      'none': '',
+      'correct': 'green',
+      'wrong': 'red',
+    };
+    return mapping[highlight];
+  }
+
   return (
-    <div className="row turn" style={{ backgroundColor: "white" }}>
+    <div className="row turn" style={{ backgroundColor: highlightToBgColor(highlight)}}>
       <div className="col-4 offset-1">
-        <img src={author.imageUrl} className="authorimage" alt="Author" />
+        <img src={author.imageUrl} className="authorImage" alt="Author" />
       </div>
       <div className="col-5 offset-1">
-        {books.map((title) => <Book title={title} key={title} />)}
+        {books.map((title) => <Book title={title} key={title} onClick={onAnswerSelected}/>)}
       </div>
     </div>
   );
+}
+
+Turn.propTypes = {
+  author: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imageUrl: PropTypes.string.isRequired,
+    imageSource: PropTypes.string.isRequired,
+    books: PropTypes.arrayOf(PropTypes.string).isRequired,
+    onAnswerSelected: PropTypes.func.isRequired,
+    highlight: PropTypes.string.isRequired,
+  }),
 }
 
 function Continue() {
@@ -48,11 +69,11 @@ function Footer() {
   );
 }
 
-function AuthorQuiz({ turnData }) {
+function AuthorQuiz({ turnData, highlight, onAnswerSelected }) {
   return (
     <div className="container-fluid">
       <Hero />
-      <Turn {...turnData} />
+      <Turn {...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected} />
       <Continue />
       <Footer />
     </div>
